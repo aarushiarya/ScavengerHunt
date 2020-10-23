30093931e4d6cf8d24ab939480334d07bd8f5057
Aarushi Arya <aarushiarya25@gmail.com>
2020-10-23 19:50:52 -0400

    Create Clue5 solution python script

---
+++ Clue5/Clue5solution.py
@@ -0,0 +1,31 @@
+# Python program to implement Reverse Alphabet Decoder 
+  
+''' 
+VARIABLE 
+'decoded_string' -> 'stores the decoded string' 
+'message' -> 'stores the string to be encoded or decoded' 
+'''
+  
+# Dictionary representing the reverse alphabet chart 
+REVERSE_ALPHABET_CODE_DICT = { 'A':'Z', 'B':'Y', 
+                    'C':'X', 'D':'W', 'E':'V', 
+                    'F':'U', 'G':'T', 'H':'S', 
+                    'I':'R', 'J':'Q', 'K':'P', 
+                    'L':'O', 'M':'N', 'N':'M', 
+                    'O':'L', 'P':'K', 'Q':'J', 
+                    'R':'I', 'S':'H', 'T':'G', 
+                    'U':'F', 'V':'E', 'W':'D', 
+                    'X':'C', 'Y':'B', 'Z':'A'} 
+
+# Decrypt the string from reverse alphabets
+
+message = 'URIHGDLNZMGLDRMGSVZ.N.GFIRMTZDZIWULIXLNKFGRMT'
+
+decoded_string = '' 
+
+for letter in message:
+   # print letter
+   if letter in REVERSE_ALPHABET_CODE_DICT:
+       decoded_string+= REVERSE_ALPHABET_CODE_DICT.get(letter)
+  
+print decoded_string
